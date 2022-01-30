e74065242990afc2f8eadbbd63717165767bd10c
vineboneto <vineboneto@gmail.com>
2022-01-29 21:11:34 -0300

    feat: update gitignore

--- App.js
+++ App.js
@@ -1,113 +1,98 @@
-import React, { useState, useEffect } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Pressable, ScrollView } from "react-native";
-import * as WebBrowser from "expo-web-browser";
-import * as Google from "expo-auth-session/providers/google";
-import { makeRedirectUri } from "expo-auth-session";
-import * as Linking from "expo-linking";
-import Constants from "expo-constants";
-
-// WebBrowser.maybeCompleteAuthSession();
+import React, { useState, useEffect } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Pressable, ScrollView } from 'react-native'
+import * as Google from 'expo-auth-session/providers/google'
+import Constants from 'expo-constants'
 
 export default function App() {
-  const [user, setUser] = useState({});
-  const [error, setError] = useState("");
+  const [user, setUser] = useState({})
+  const [error, setError] = useState('')
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    expoClientId:
-      "1061294397430-plh7m08rnor0ttcvuu661u0mpe2vkip8.apps.googleusercontent.com",
-    androidClientId:
-      "1061294397430-6g6bt0iu3n8fsjkje2u4tscuqs8bim21.apps.googleusercontent.com",
-    ...(Constants.appOwnership !== "expo"
-      ? { redirectUri: "com.temp://" }
-      : {}),
-  });
+    expoClientId: process.env.EXPO_ID,
+    androidClientId: process.env.ANDROID_ID,
+    ...(Constants.appOwnership === 'expo'
+      ? { redirectUri: process.env.EXPO_REDIRECT_URI }
+      : { redirectUri: process.env.EXPO_REDIRECT_URI_PRODUCT }),
+  })
 
   useEffect(() => {
-    async function handleSignGoogle() {
+    async function signInWithGoogle() {
       try {
-        if (
-          response?.type === "success" &&
-          response.authentication?.accessToken
-        ) {
-          const data = await fetch(
-            `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${response.authentication.accessToken}`
-          );
-          const userInfo = await data.json();
+        if (response.type === 'success' && response?.authentication?.accessToken) {
+          const baseUrl = 'https://www.googleapis.com/oauth2/v1/userinfo'
+          const dataFetch = await fetch(`${baseUrl}?alt=json&access_token=${response.authentication.accessToken}`)
+          const userInfo = await dataFetch.json()
           setUser({
             id: userInfo.id,
             email: userInfo.email,
             name: userInfo.given_name,
             photo: userInfo.picture,
-          });
+          })
         }
       } catch (err) {
-        setError(err.message);
+        setError(err.message)
       }
     }
-    handleSignGoogle();
-  }, [response]);
+    signInWithGoogle()
+  }, [response])
 
   return (
     <View style={styles.content}>
       <ScrollView style={styles.container}>
         <StatusBar style="auto" />
         <Text>Testando app {process.env.EXPO_APP_NAME}</Text>
-        {!!user?.name && (
-          <Text style={styles.textWelcome}>Bem vindo, {user.name}</Text>
-        )}
-        {!!error && <Text style={styles.textWelcome}>{error}</Text>}
+        {!!user?.name && <Text style={styles.textWelcome}>Bem vindo, {user.name}</Text>}
+        <Text style={styles.debug}>error: {error}</Text>
         <Pressable
           style={({ pressed }) => [
             {
-              backgroundColor: pressed ? "#421549" : "purple",
+              backgroundColor: pressed ? '#421549' : 'purple',
             },
             styles.button,
           ]}
-          onPress={() => {
-            promptAsync();
-          }}
+          onPress={promptAsync}
         >
           <Text style={styles.buttonText}>Fazer Login com google</Text>
         </Pressable>
       </ScrollView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
   content: {
     flex: 1,
     marginTop: 60,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   button: {
     borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: "#ececec",
+    borderStyle: 'solid',
+    borderColor: '#ececec',
     height: 60,
     marginTop: 20,
-    alignItems: "center",
-    justifyContent: "center",
-    color: "#fff",
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#fff',
     paddingHorizontal: 20,
   },
   debug: {
-    textAlign: "center",
+    textAlign: 'center',
     paddingHorizontal: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   buttonText: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 20,
   },
   textWelcome: {
     fontSize: 40,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
-});
+})
---
+++ readme.md
@@ -0,0 +1,69 @@
+## Autenticação Google
+
+- Estudo de como realizar login social `google` utilizando expo
+
+# Requisitos
+
+- NODEJS [https://nodejs.org/en/download/](Link aqui)
+- JavaSDK
+- bundletool [https://github.com/google/bundletool/releases](Link aqui)
+- Expo instalado [https://docs.expo.dev/get-started/installation/](Link aqui)
+- Logado em sua conta expo [https://docs.expo.dev/workflow/expo-cli/#auth](Link aqui)
+
+# Variáveis de ambiente
+
+- Para configurar as variáveis de ambiente será necessário criar as credencias de client auth na `google`,
+  recomendo fortemente seguir a documentação presente no site da expo [https://docs.expo.dev/guides/authentication/#google](aqui), a descrição das mesmas se encontra abaixa de acordo com a documentação proposta no site
+- Preste atenção no arquivo `app.json`, possui informações importantes a respeito de build, é essencial configurar o `scheme` e `slug` para definir a variável de ambiente `EXPO_REDIRECT_URI`
+- A `redirect_uri`, é encarregado de retornar para o app após realizar a autenticação com o google
+- A `redirect_uri` utiliza de valores diferentes de acordo com o ambiente que está sendo executado, por exemplo, se você está executando o app com expo a `redirect_uri` possuíra um valor diferente de um app já instalado em um `android` ou `ios`.
+- Para o ambiente que o app será instalado é necessário configurações nas pastas `android` e `ios`, neste projeto já está configurado uma `redirect_uri` chamada `com.temp://`, porém você pode realizar está configuração com [https://www.npmjs.com/package/uri-scheme](uri-scheme), executando `npx uri-scheme list` para listar todas as uri do projeto e `npx uri-scheme add <your_uri>` para adicionar uma nova uri, está biblioteca se encarregará de realizar as configurações necessárias nas pastas `android` e `ios`, caso queira saber mais informações pode consultar esta [documentação](https://github.com/expo/expo/tree/main/packages/expo-auth-session#configuration)
+
+```shell
+# Nome do app, um tanto irrelevante
+EXPO_APP_NAME=temp
+# Representa o  expoClientId
+EXPO_ID=
+# Representa o androidClientId
+EXPO_ANDROID_ID=
+# Representa a URL de redirect no modo develop com expo client, segue abaixo exemplo
+EXPO_REDIRECT_URI=https://auth.expo.io/@your-username-expo-account/yout-slug-expo-project
+# Representa a URL de redirect quando o app for instalado em um android ou ios
+EXPO_REDIRECT_URI_PRODUCT=com.temp://
+```
+
+# Instalação Develop
+
+```shell
+# Instalar dependências
+$ yarn
+
+# Executar com expo client
+$ expo start
+
+```
+
+# Produção
+
+```shell
+# Instalar dependências
+$ yarn
+
+# Para gerar o build será necessário instalar o eas
+$ npx install -g eas
+
+# Depois o build pode ser gerado através do comando
+$ yarn build:android
+# Após gerado, você poderá realizar download através de um link de um arquivo .aab
+
+# Esse arquivo poderá ser convertido para um arquivo .apks através do bundletool
+# É importante executar este comando com o java devidamente configurado nas variáveis de ambiente do seu sistema
+# E com o arquivo bundletoo.jar e o arquivo .aab gerado através do build no mesmo diretório
+java -jar bundletool.jar build-apks --bundle=my-app.aab --output=my_app.apks --mode=universal
+
+# Tendo o arquivo .apks, você pode transforma-lo em um arquivo .zip manualmente, apenas renomeando-o.
+# E então realizar a extração dos arquivos dentro desse .zip criado, dentro conterá um arquivo .apk.
+# E com esse arquivo você poderá realizar a instalação em seu dispositivo android
+
+
+```
